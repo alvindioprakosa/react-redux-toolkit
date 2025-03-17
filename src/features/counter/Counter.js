7a4e06e90b6fe1511d624702bd13bcac97b1cced
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 09:59:16 +0700

    Update Counter

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,27 +3,25 @@ import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./counterSlice";
 
 export function Counter() {
+  // Use useSelector to access the counter state
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
   return (
     <div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          aria-label="Increment Value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <button
-          aria-label="Decrement Value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+      <h1>{count}</h1>
+      <button
+        aria-label="Increment Value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <button
+        aria-label="Decrement Value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
     </div>
   );
 }
-
