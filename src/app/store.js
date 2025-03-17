fa21b4cfe92469377b2c27cc29fb9756122684dc
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 09:59:38 +0700

    Add Comment Counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
+// Create Redux store and set the counter reducer
 export const store = configureStore({
   reducer: {
-      counter: counterReducer,
+    counter: counterReducer,
   },
-})
+});
+
+// Optional: If you’re using TypeScript, you can export the store's types for type safety
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
