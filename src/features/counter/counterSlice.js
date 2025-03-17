0034f702244816fd9fd1ffc09bc649dfc7fb78c2
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 09:58:32 +0700

    Update Counter Code

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,25 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Initial state of the counter
 const initialState = {
     value: 0,
 }
 
+// Create slice for counter actions
 export const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
         increment: (state) => {
-            state.value += 1;
+            state.value += 1;  // Increment by 1
         },
         decrement: (state) => {
-            state.value -= 1;
+            state.value -= 1;  // Decrement by 1
         },
         incrementByAmount: (state, action) => {
-            state.value += action.payload
+            state.value += action.payload;  // Increment by specified amount
         }
     }
 })
 
-export const {increment, decrement, incrementByAmount} = counterSlice.actions
+// Export actions for use in components
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
+// Export the reducer to be used in the store
 export default counterSlice.reducer;
